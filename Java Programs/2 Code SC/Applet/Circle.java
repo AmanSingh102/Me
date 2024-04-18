8266dc21bb2347ffa896683698bacb78da03e8c7
Aman Singh <161552125+AmanSingh102@users.noreply.github.com>
2024-04-18 15:55:27 +0530

    Add files via upload

---
+++ Java Programs/2 Code SC/Applet/Circle.java
@@ -0,0 +1,23 @@
+import java.applet.*;
+import java.awt.*;
+
+/* 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Circle Shape</title>
+</head>
+<body>
+    <applet code="Circle" width="300" height="200"> </applet>
+</body>
+</html>
+*/
+
+public class Circle extends Applet
+{
+    public void paint( Graphics graphics )
+    {
+        graphics.setColor(Color.green);
+        graphics.fillOval(200, 200, 200, 200);
+    }
+}
