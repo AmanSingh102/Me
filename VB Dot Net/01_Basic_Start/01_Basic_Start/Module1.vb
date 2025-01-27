1b2dc7defb23432b1241b9469d00cc274d4bf1ad
Aman Singh <161552125+AmanSingh102@users.noreply.github.com>
2025-01-27 20:05:01 +0530

    Add files via upload

---
+++ VB Dot Net/01_Basic_Start/01_Basic_Start/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello World")    'This print a new line'
+        Console.Write("Hello World")        'This print without a new line'
+        Console.ReadKey()
+    End Sub
+
+End Module
+
+'Output -------------------------------------------------
+
+'Hello World
+'Hello World
+
+
