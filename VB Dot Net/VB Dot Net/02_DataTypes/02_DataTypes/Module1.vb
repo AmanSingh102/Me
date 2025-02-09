669e7f24172f887300f85cfed2070c8ff770ad9d
Aman Singh <161552125+AmanSingh102@users.noreply.github.com>
2025-02-09 20:21:46 +0530

    Add files via upload

---
+++ VB Dot Net/VB Dot Net/02_DataTypes/02_DataTypes/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim b As Byte
+        Dim n As Integer
+        Dim si As Single
+        Dim d As Double
+        Dim da As Date
+        Dim c As Char
+        Dim s As String
+        Dim bl As Boolean
+
+        b = 2
+        n = 1234567
+        si = 0.1234567891
+        d = 0.12345678911223344
+        da = Today
+        c = "U"c
+        s = "Me"
+
+        If ScriptEngine = "VB" Then
+            bl = True
+        Else
+            bl = False
+        End If
+
+        If bl Then
+            Console.Write(c & " and, " & s & vbCrLf)
+            Console.WriteLine("declaring on the day of : {0}", da)
+            Console.WriteLine("We will learn VB.net seriously")
+            Console.WriteLine("Lets see what happens to the floating point variables : ")
+            Console.WriteLine("The Single : {0}, The Double : {1}", si, d)
+        End If
+
+        Console.ReadKey()
+    End Sub
+
+End Module
+
+'Output -------------------------------------------------------
+
+'U And, Me
+'declaring on the day of : 27-01-2025 00:00:00
+'We will learn VB.net seriously
+'Lets see what happens To the floating point variables 
+'The Single :  0.1234568, The Double : 0.123456789112233
---
+++ VB Dot Net/VB Dot Net/02_DataTypes/02_DataTypes/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("02_DataTypes")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("02_DataTypes")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5ddae0d4-00d8-4dbb-9dd1-7d344efe225a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
