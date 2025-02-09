fa49014e7801f7f150f49c47d4f41671a91399df
Aman Singh <161552125+AmanSingh102@users.noreply.github.com>
2025-02-09 20:25:30 +0530

    Add files via upload

---
+++ VB Dot Net/VB Dot Net/05_Add_Two_Numbers/Add_Two_Numbers/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num1 As Integer = 5
+        Dim num2 As Integer = 10
+        Dim result As Integer = num1 + num2
+
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, result)
+
+        Console.WriteLine(vbCrLf & "---- Extended Version ----")   'Environment.NewLine equal to vbCrLf' 
+        Dim n1 As Integer
+        Dim n2 As Integer
+        Dim sum As Integer
+
+        n1 = 4
+        n2 = 6
+        sum = n1 + n2
+
+        Console.WriteLine("a = {0}, b = {1}, c = {2}", n1, n2, sum)
+        Console.ReadLine()
+    End Sub
+
+End Module
+
+
+'Output -----------------------------------------------------
+
+'5 + 10 = 15
+
+'---- Extended Version ----
+'a = 4, b = 6, c = 10
---
+++ VB Dot Net/VB Dot Net/06_Accepting_Value_From_User/06_Accepting_Value_From_User/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("06_Accepting_Value_From_User")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("06_Accepting_Value_From_User")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("22945896-d29e-4b80-bd13-4c0a40e3049d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
