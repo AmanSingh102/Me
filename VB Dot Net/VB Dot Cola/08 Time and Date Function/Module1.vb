ac4680276d7cd974fbb4818cfd4d22e3d6640493
Aman Singh <161552125+AmanSingh102@users.noreply.github.com>
2025-04-15 15:20:34 +0530

    Add files via upload

---
+++ VB Dot Net/VB Dot Cola/08 Time and Date Function/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+    Sub Main()
+
+        Console.WriteLine("Current Date & Time: " & Now)
+        Console.WriteLine("Year: " & Year(Now))
+        Console.WriteLine("Month: " & Month(Now))
+        Console.WriteLine("Day: " & Day(Now))
+        Console.WriteLine("Hour: " & Hour(Now))
+        Console.WriteLine("Minute: " & Minute(Now))
+        Console.WriteLine("Second: " & Second(Now))
+
+        Console.WriteLine("Date after 10 days: " & DateAdd("d", 10, Now))
+        Console.WriteLine("Days between 1st Jan 2025 and today: " & DateDiff("d", #1/1/2025#, Now))
+
+    End Sub
+End Module
+
+
+'OUTPUT
+'Current Date & Time:  11-04-2025 16:08:06
+'Year: 2025
+'Month: 4
+'Day: 11
+'Hour: 16
+'Minute: 8
+'Second: 6
+'Date after 10 days:  21-04-2025 16:08:06
+'Days between 1St Jan 2025 And today: 100
